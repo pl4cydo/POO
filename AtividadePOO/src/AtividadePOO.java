c8519562a058129a1914ee7abbb59c5b07f54dda
Plácydo Lima <105952926+pl4cydo@users.noreply.github.com>
2022-08-26 09:14:47 -0300

    Atividade

---
+++ AtividadePOO/src/AtividadePOO.java
@@ -0,0 +1,77 @@
+
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class AtividadePOO {
+	public static void main(String[] args) {
+		
+//		Scanner sc = new Scanner(System.in);
+		
+//		SEGUNDO
+//		float imc;
+//		float peso;
+//		float altura;
+//		
+//		System.out.println("Insira seu peso em Kg: ");
+//		peso = sc.nextFloat();
+//		
+//		System.out.println("Insira altura: ");
+//		altura = sc.nextFloat();
+//		
+//		imc = peso/(altura*altura);
+//		System.out.print("Seu imc é " + imc);
+//	
+//		TERCEIRO
+//		 float baseMenor;
+//		 float baseMaior;
+//		 float altura;
+//		 float resultado;
+//		 
+//		 System.out.println("Insira a Base Menor");
+//		 baseMenor = sc.nextFloat();
+//		 
+//		 System.out.println("Insira a Base Maior");
+//		 baseMaior = sc.nextFloat();
+//		 
+//		 System.out.println("Insira a altura");
+//		 altura = sc.nextFloat();
+//		 
+//		 resultado = ((baseMenor + baseMenor) * altura)/2;   
+//		 System.out.println("O resultado " + resultado);
+//		
+		
+//		QUARTO
+//		int num1;
+//		int num2;
+//			
+//		System.out.println("Insira o primeiro inteiro");
+//		num1 = sc.nextInt();
+//		
+//		System.out.println("Insira o segundo inteiro");
+//		num2 = sc.nextInt();
+//		
+//		if(num1 > num2) {
+//			System.out.println("O " + num1 + " é o maior");
+//		} else {
+//			System.out.println("O " + num2 + " é o maior");
+//		}
+		
+//		QUINTO
+//		int num;
+//		
+//		System.out.println("Digite seu numero");
+//		num = sc.nextInt();
+//		
+//		if (num % 2 == 0) {
+//			System.out.println(num + " é par");
+//		} else {
+//			System.out.println(num + " é impar");
+//		}
+		
+		
+//	
+		
+		
+	}
+}
+
---
+++ AtividadePOO/src/Ficha2.java
@@ -0,0 +1,71 @@
+
+public class Ficha2 {
+	public static void main(String[] args) {
+	
+//	*************************** SEGUNDA FICHA************
+//	Scanner placydo = new Scanner(System.in);
+//	
+//	float nota1;
+//	float nota2;
+//	float nota3;
+//	float nota4;
+//	float nota5;
+//	float resultado;
+//	
+//	System.out.println("Digite suas notas");
+//	nota1 = placydo.nextFloat();
+//	nota2 = placydo.nextFloat();
+//	nota3 = placydo.nextFloat();
+//	nota4 = placydo.nextFloat();
+//	nota5 = placydo.nextFloat();
+//	
+//	resultado = (nota1 + nota2 + nota3 + nota4 + nota5) / 5;
+//	System.out.println("A média é: " + resultado);
+//	
+//	SEGUNDO
+//	int[] array = new int[10];
+//	
+//	array [0] = 0;
+//	array [1] = 1;
+//	array [2] = 2;
+//	array [3] = 3;
+//	array [4] = 4;
+//	array [5] = 5;
+//	array [6] = 6;
+//	array [7] = 7;
+//	array [8] = 8;
+//	array [9] = 9;
+//	
+//
+//	int i;
+//	
+//	for(i=array.length - 1; i >= 0; i--) {
+//		System.out.println(array[i]);
+//	}
+	
+//	TERCEIRO
+//	int[] arrayBase;
+//	arrayBase = new int[ 100 ];
+//	System.out.printf("%s %10s \n", "Index", "Valores");
+//	for(int i = 0; i < arrayBase.length; i++) {
+//		arrayBase[i] = i;
+//		System.out.printf("%3d %10d \n", i, arrayBase[ i ]);
+//	}
+//	QUARTO
+//		int[] arrayBase;
+//		int cont = 0;
+//		arrayBase = new int[ 100 ];
+//		for(int i = 0; i < arrayBase.length; i++) {
+//			arrayBase[i] = i;
+//			for(int j = 2; j < i; j++) {
+//				if(i%j == 0) {
+//					cont++;
+//				}
+//			if (cont == 0) {
+//				System.out.print(arrayBase[i]);
+//			}
+//			}
+//		}
+//	}
+//}
+	
